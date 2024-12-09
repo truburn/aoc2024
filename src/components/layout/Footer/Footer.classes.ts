ebf731ffc2a1612e79dbcc34013405d0e4db50df
Jenny Seburn <jennifer.seburn@gmail.com>
2024-12-09 11:47:43 -0500

    setup footer

--- src/components/layout/Footer/Footer.classes.ts
+++ src/components/layout/Footer/Footer.classes.ts
@@ -1,14 +1,37 @@
 import { css } from "@emotion/css";
 import { useTheme } from "@emotion/react";
-import { borderMixin, paddingMixin } from "@styles/mixins";
+import { marginMixin, paddingMixin } from "@styles/mixins";
 
 export function useFooterClasses() {
   const theme = useTheme();
 
   const root = css({
-    ...borderMixin(theme.border),
-    ...paddingMixin(16),
+    ...paddingMixin({ vertical: 8, horizontal: 24 }),
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "stretch",
+    justifyContent: "space-between",
+    background: theme.color.fg,
+    color: theme.color.bg,
+    fontSize: theme.font.standard.size.medium,
+    lineHeight: 1,
+    "& > div": {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      gap: "1ch",
+    },
   });
 
-  return { root };
+  const link = css({
+    "& svg": {
+      fontSize: theme.font.standard.size.large,
+      ...marginMixin(4),
+    },
+    "&:hover": {
+      color: theme.color.bg,
+    },
+  });
+
+  return { root, link };
 }
--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,7 +1,41 @@
+import Link from "@components/Link";
 import { useFooterClasses } from "@layout/Footer";
 
 export function Footer() {
   const classes = useFooterClasses();
 
-  return <div className={classes.root}>Footer Component</div>;
+  return (
+    <footer className={classes.root}>
+      <div>
+        <i className="fa-light fa-copyright" />
+        Copyright 2024{" "}
+        <Link
+          to="https://www.jsburn.com/"
+          external
+          title="View my work"
+          className={classes.link}
+        >
+          Jenny Seburn
+        </Link>
+      </div>
+      <div>
+        <Link
+          to="https://github.com/truburn/aoc2024"
+          external
+          title="View GitHub Repository"
+          className={classes.link}
+        >
+          <i className="fa-brands fa-github"></i>
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/jenniferseburn"
+          external
+          title="Connect on LinkedIn"
+          className={classes.link}
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </Link>
+      </div>
+    </footer>
+  );
 }
