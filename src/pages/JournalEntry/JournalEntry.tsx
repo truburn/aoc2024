f415d47b6f97af269c461260465a6946dfdf63a7
Jenny Seburn <jennifer.seburn@gmail.com>
2024-12-21 10:47:28 -0500

    fix file name casing

--- src/pages/JournalEntry/JournalEntry.tsx
+++ src/pages/JournalEntry/JournalEntry.tsx
@@ -1,17 +1,12 @@
 import DynamicDemo from "@entries/DynamicDemo";
 import DynamicEntry from "@entries/DynamicEntry";
 import { useJournalEntryClasses } from "@pages/JournalEntry";
-import { useEffect } from "react";
 import { useLoaderData, useParams } from "react-router";
 
 export function JournalEntry() {
   const classes = useJournalEntryClasses();
   const title = useLoaderData();
   const params = useParams();
-
-  useEffect(() => {
-    console.log({ title, params });
-  }, [title, params]);
 
   return (
     <div className={classes.root}>
