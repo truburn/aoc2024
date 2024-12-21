6e408f3a21e76be06ef0db49f34e4ecc54e008eb
Jenny Seburn <jennifer.seburn@gmail.com>
2024-12-20 21:35:48 -0500

    fill out home page

--- src/pages/Home/Home.classes.ts
+++ src/pages/Home/Home.classes.ts
@@ -6,9 +6,25 @@ export function useHomeClasses() {
   const theme = useTheme();
 
   const root = css({
-    ...borderMixin(theme.border),
-    ...paddingMixin(16),
+    "&&": {
+      ...borderMixin(theme.border),
+      ...paddingMixin(16),
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 64,
+    },
   });
 
-  return { root };
+  const calendar = css({
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: 500,
+    "& a": {
+      fontSize: theme.font.monospace.size.xlarge,
+    },
+  });
+
+  return { root, calendar };
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,38 @@
+import Calendar from "@components/Calendar";
+import Link from "@components/Link";
+import NoteCard from "@components/NoteCard";
 import { useHomeClasses } from "@pages/Home";
 
 export function Home() {
   const classes = useHomeClasses();
 
-  return <div className={classes.root}>Home Page</div>;
+  return (
+    <div className={classes.root}>
+      <NoteCard
+        title="The Chief Historian is Missing!"
+        subtitle="Advent of Code 2024"
+      >
+        <p>
+          The Chief Historian for the North Pole has gone missing and he needs
+          to be found before the big Sleigh Ride.
+        </p>
+        <p>
+          There are 25 days left until the big day, and each day I find a new
+          clue that needs to be solved.
+        </p>
+        <p>Follow along on my journey to find him and get him back in time!</p>
+        <p>For each clue I solve, a journal entry will be available.</p>
+        <p>
+          If you are interested in learning more or giving it a try, visit{" "}
+          <Link to="https://adventofcode.com/" external>
+            Advent of Code
+          </Link>
+          .
+        </p>
+      </NoteCard>
+      <div className={classes.calendar}>
+        <Calendar />
+      </div>
+    </div>
+  );
 }
